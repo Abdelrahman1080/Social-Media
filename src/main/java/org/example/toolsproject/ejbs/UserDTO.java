1eef365c96d1df2f4c19676d1b9618e6d1dffb12
Ziad Ahmad Abdelrahman <68442256+ziad2292@users.noreply.github.com>
2025-04-30 11:52:19 +0300

    my commit

---
+++ src/main/java/org/example/toolsproject/ejbs/UserDTO.java
@@ -0,0 +1,23 @@
+package org.example.toolsproject.ejbs;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UserDTO {
+    public int id;
+    public String name;
+    public String email;
+    public String bio;
+    public List<UserFriendDTO> friends;
+
+    public UserDTO(User user) {
+        this.id = user.getId();
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.bio = user.getBio();
+        this.friends = user.getFriends().stream()
+                .map(UserFriendDTO::new)
+                .collect(Collectors.toList());
+    }
+}
+
